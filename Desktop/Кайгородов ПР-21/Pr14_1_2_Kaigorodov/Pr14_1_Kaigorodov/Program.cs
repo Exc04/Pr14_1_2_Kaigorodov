89fd0afbcdd809ee8181460daec3b34a5f0baaf8
sogdian <tarssvisionkira@gmail.com>
2026-03-13 15:38:16 +0500

    Практика 14

---
+++ Desktop/Кайгородов ПР-21/Pr14_1_2_Kaigorodov/Pr14_1_Kaigorodov/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr14_1_Kaigorodov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите значение n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (n <= 0)
+                {
+                    Console.WriteLine("Число должно быть больше 0");
+                    return;
+                }
+                Stack<int> A = new Stack<int>(n);
+                for ( int i = 1; i <= n; i++)
+                {
+                    A.Push(i);
+                }
+                Console.WriteLine($"n = {n}");
+                Console.WriteLine($"Размерность стека {A.Count}");
+                Console.WriteLine($"Верхний элемент стека = {A.Peek()}");
+                string st = "";
+                for (int i = 0; i < n; i++)
+                {
+                    st += $"{A.Pop()} ";
+                }
+                Console.WriteLine($"Содержимое стека = {st}");
+                Console.WriteLine($"Новая размерность стека {A.Count}");
+
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Ошибка неверный тип данных");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Desktop/Кайгородов ПР-21/Pr14_1_2_Kaigorodov/Pr14_2_Kaigorodov/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr14_2_Kaigorodov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool Error = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vir = textBox1.Text;
+            Null(vir);
+            if (Error) return;
+            File.WriteAllText("t.txt", vir);
+            int ErrPos = -1;
+            bool Balance = CheckBalance(vir, out ErrPos);
+            
+            if (Balance)
+            {
+                MessageBox.Show("Скобки сбалансированы");
+            }
+            else
+            {
+                if (ErrPos != -1)
+                {
+                    MessageBox.Show($"Возможно лишняя скобка в позиции: {ErrPos}");
+                }
+                else
+                {
+                    MessageBox.Show("Скобки не сбалансированы(не хватает закрывющих скобок)");
+                }
+            }
+            
+        }
+
+        static bool CheckBalance (string text, out int ErrPos)
+        {
+            int balance = 0;
+            ErrPos = -1;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (c == '(')
+                {
+                    balance++;
+                }
+                else if (c == ')')
+                {
+                    balance--;
+                    if (balance < 0)
+                    {
+                        ErrPos = i + 1;
+                        return false;
+                    }
+                }
+            }
+            return balance == 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string vir = textBox1.Text;
+            Null(vir);
+            if (Error) return;
+            string Fix = FixBalance(vir);
+            File.WriteAllText("t1.txt", Fix);
+            textBox1.Text = Fix;
+        }
+        
+        public void  Null (string text)
+        {
+            if (text.Length == 0)
+            {
+                MessageBox.Show("Строка пуста!");
+                Error = true;
+                return;
+            }
+            int count = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((!Char.IsDigit(text[i])) && (text[i] != '+') && (text[i] != '-') && (text[i] != '*') && (text[i] != '/') && (text[i] != '=') && (text[i] != ' ') && (text[i] != '(') && (text[i] != ')'))
+                {
+                    MessageBox.Show("Недопустимые символы в выражении!");
+                    Error = true;
+                    return;
+                }
+
+
+                if (!Char.IsDigit(text[0]) && text[0] != '(' && text[0] != '-')
+                {
+                    MessageBox.Show("первый символ должен быть цифра, ( или -");
+                    Error = true;
+                    return;
+                }
+               
+            }
+        }
+
+        static string FixBalance (string text)
+        {
+            int balance = 0;
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in text)
+            {
+                if (c == '(')
+                {
+                    balance++;
+                    result.Append(c);
+                }
+                else 
+                {
+                    if (c == ')')
+                    {
+                        if (balance > 0)
+                        {
+                            balance--;
+                            result.Append(c);
+                        }
+                    }
+                    else
+                    {
+                        result.Append(c);
+                    }
+                }
+            }
+
+            for (int i = 0; i < balance; i++)
+            {
+                result.Append(')');
+            }
+
+            return result.ToString();
+        }
+    }
+}
